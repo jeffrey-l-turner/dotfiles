6bacfdebd2dd75a144e416e294a4dec33281a11d
jeffrey-l-turner <Jeffrey.L.Turner@me.com>
2014-11-23 10:48:30 -0800

    changed to locals

--- colordefs.sh
+++ colordefs.sh
@@ -9,91 +9,95 @@
 function color () {
     arg1=$1
 
-    declare Color_Off="\[\033[0m\]"       # Text Reset
+    local declare Color_Off="\[\033[0m\]"       # Text Reset
 
     # Regular Colors
-    declare Black="\[\033[0;30m\]"        # Black
-    declare Red="\[\033[0;31m\]"          # Red
-    declare Green="\[\033[0;32m\]"        # Green
-    declare Yellow="\[\033[0;33m\]"       # Yellow
-    declare Blue="\[\033[0;34m\]"         # Blue
-    declare Purple="\[\033[0;35m\]"       # Purple
-    declare Cyan="\[\033[0;36m\]"         # Cyan
-    declare White="\[\033[0;37m\]"        # White
+    local declare Black="\[\033[0;30m\]"        # Black
+    local declare Red="\[\033[0;31m\]"          # Red
+    local declare Green="\[\033[0;32m\]"        # Green
+    local declare Yellow="\[\033[0;33m\]"       # Yellow
+    local declare Blue="\[\033[0;34m\]"         # Blue
+    local declare Purple="\[\033[0;35m\]"       # Purple
+    local declare Cyan="\[\033[0;36m\]"         # Cyan
+    local declare White="\[\033[0;37m\]"        # White
 
     # Bold
-    declare BBlack="\[\033[1;30m\]"       # Black
-    declare BRed="\[\033[1;31m\]"         # Red
-    declare BGreen="\[\033[1;32m\]"       # Green
-    declare BYellow="\[\033[1;33m\]"      # Yellow
-    declare BBlue="\[\033[1;34m\]"        # Blue
-    declare BPurple="\[\033[1;35m\]"      # Purple
-    declare BCyan="\[\033[1;36m\]"        # Cyan
-    declare BWhite="\[\033[1;37m\]"       # White
+    local declare BBlack="\[\033[1;30m\]"       # Black
+    local declare BRed="\[\033[1;31m\]"         # Red
+    local declare BGreen="\[\033[1;32m\]"       # Green
+    local declare BYellow="\[\033[1;33m\]"      # Yellow
+    local declare BBlue="\[\033[1;34m\]"        # Blue
+    local declare BPurple="\[\033[1;35m\]"      # Purple
+    local declare BCyan="\[\033[1;36m\]"        # Cyan
+    local declare BWhite="\[\033[1;37m\]"       # White
     
     # Underline
-    declare UBlack="\[\033[4;30m\]"       # Black
-    declare URed="\[\033[4;31m\]"         # Red
-    declare UGreen="\[\033[4;32m\]"       # Green
-    declare UYellow="\[\033[4;33m\]"      # Yellow
-    declare UBlue="\[\033[4;34m\]"        # Blue
-    declare UPurple="\[\033[4;35m\]"      # Purple
-    declare UCyan="\[\033[4;36m\]"        # Cyan
-    declare UWhite="\[\033[4;37m\]"       # White
+    local declare UBlack="\[\033[4;30m\]"       # Black
+    local declare URed="\[\033[4;31m\]"         # Red
+    local declare UGreen="\[\033[4;32m\]"       # Green
+    local declare UYellow="\[\033[4;33m\]"      # Yellow
+    local declare UBlue="\[\033[4;34m\]"        # Blue
+    local declare UPurple="\[\033[4;35m\]"      # Purple
+    local declare UCyan="\[\033[4;36m\]"        # Cyan
+    local declare UWhite="\[\033[4;37m\]"       # White
     
     # Background
-    declare On_Black="\[\033[40m\]"       # Black
-    declare On_Red="\[\033[41m\]"         # Red
-    declare On_Green="\[\033[42m\]"       # Green
-    declare On_Yellow="\[\033[43m\]"      # Yellow
-    declare On_Blue="\[\033[44m\]"        # Blue
-    declare On_Purple="\[\033[45m\]"      # Purple
-    declare On_Cyan="\[\033[46m\]"        # Cyan
-    declare On_White="\[\033[47m\]"       # White
+    local declare On_Black="\[\033[40m\]"       # Black
+    local declare On_Red="\[\033[41m\]"         # Red
+    local declare On_Green="\[\033[42m\]"       # Green
+    local declare On_Yellow="\[\033[43m\]"      # Yellow
+    local declare On_Blue="\[\033[44m\]"        # Blue
+    local declare On_Purple="\[\033[45m\]"      # Purple
+    local declare On_Cyan="\[\033[46m\]"        # Cyan
+    local declare On_White="\[\033[47m\]"       # White
     
     # High Intensty
-    declare IBlack="\[\033[0;90m\]"       # Black
-    declare IRed="\[\033[0;91m\]"         # Red
-    declare IGreen="\[\033[0;92m\]"       # Green
-    declare IYellow="\[\033[0;93m\]"      # Yellow
-    declare IBlue="\[\033[0;94m\]"        # Blue
-    declare IPurple="\[\033[0;95m\]"      # Purple
-    declare ICyan="\[\033[0;96m\]"        # Cyan
-    declare IWhite="\[\033[0;97m\]"       # White
+    local declare IBlack="\[\033[0;90m\]"       # Black
+    local declare IRed="\[\033[0;91m\]"         # Red
+    local declare IGreen="\[\033[0;92m\]"       # Green
+    local declare IYellow="\[\033[0;93m\]"      # Yellow
+    local declare IBlue="\[\033[0;94m\]"        # Blue
+    local declare IPurple="\[\033[0;95m\]"      # Purple
+    local declare ICyan="\[\033[0;96m\]"        # Cyan
+    local declare IWhite="\[\033[0;97m\]"       # White
     
     # Bold High Intensty
-    declare BIBlack="\[\033[1;90m\]"      # Black
-    declare BIRed="\[\033[1;91m\]"        # Red
-    declare BIGreen="\[\033[1;92m\]"      # Green
-    declare BIYellow="\[\033[1;93m\]"     # Yellow
-    declare BIBlue="\[\033[1;94m\]"       # Blue
-    declare BIPurple="\[\033[1;95m\]"     # Purple
-    declare BICyan="\[\033[1;96m\]"       # Cyan
-    declare BIWhite="\[\033[1;97m\]"      # White
+    local declare BIBlack="\[\033[1;90m\]"      # Black
+    local declare BIRed="\[\033[1;91m\]"        # Red
+    local declare BIGreen="\[\033[1;92m\]"      # Green
+    local declare BIYellow="\[\033[1;93m\]"     # Yellow
+    local declare BIBlue="\[\033[1;94m\]"       # Blue
+    local declare BIPurple="\[\033[1;95m\]"     # Purple
+    local declare BICyan="\[\033[1;96m\]"       # Cyan
+    local declare BIWhite="\[\033[1;97m\]"      # White
     
     # High Intensty backgrounds
-    declare On_IBlack="\[\033[0;100m\]"   # Black
-    declare On_IRed="\[\033[0;101m\]"     # Red
-    declare On_IGreen="\[\033[0;102m\]"   # Green
-    declare On_IYellow="\[\033[0;103m\]"  # Yellow
-    declare On_IBlue="\[\033[0;104m\]"    # Blue
-    declare On_IPurple="\[\033[10;95m\]"  # Purple
-    declare On_ICyan="\[\033[0;106m\]"    # Cyan
-    declare On_IWhite="\[\033[0;107m\]"   # White
-    
-    # Various variables you might want for your PS1 prompt instead
-    declare Time12h="\T"
-    declare Time12a="\@"
-    declare PathShort="\w"
-    declare PathFull="\W"
-    declare NewLine="\n"
-    declare Jobs="\j"
+    local declare On_IBlack="\[\033[0;100m\]"   # Black
+    local declare On_IRed="\[\033[0;101m\]"     # Red
+    local declare On_IGreen="\[\033[0;102m\]"   # Green
+    local declare On_IYellow="\[\033[0;103m\]"  # Yellow
+    local declare On_IBlue="\[\033[0;104m\]"    # Blue
+    local declare On_IPurple="\[\033[10;95m\]"  # Purple
+    local declare On_ICyan="\[\033[0;106m\]"    # Cyan
+    local declare On_IWhite="\[\033[0;107m\]"   # White
 
-    if [ $# -eq 1 ]; then
-        echo -n "${!arg1}"
-    else 
-        echo -n ${!arg1} | sed -e 's/\\\[//' -e 's/\\\]//' | tr -d '\n'
+    if [ $arg1 ]; then
+        if [[ $# -gt 0 && $# -lt 2 ]]; then
+            echo -n "${!arg1}"
+        else 
+            echo -n ${!arg1} | sed -e 's/\\\[//' -e 's/\\\]//' | tr -d '\n'
+        fi
     fi
+}
+    
+function Prompt () {
+    # Various variables you might want for your PS1 prompt instead
+    local declare Time12h="\T"
+    local declare Time12a="\@"
+    local declare PathShort="\w"
+    local declare PathFull="\W"
+    local declare NewLine="\n"
+    local declare Jobs="\j"
 }
 
 color  $*
