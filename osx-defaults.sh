6cec7f3f6afcb17999a0c7cc6897f8882b08a03b
Jeff Turner <Jeff.Turner@fox.com>
2015-09-02 10:57:44 -0700

    corrected screencaps location

--- osx-defaults.sh
+++ osx-defaults.sh
@@ -85,7 +85,10 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "$HOME/screencaps"
+if [ ! -f "$HOME/Desktop/screencaps" ]; then 
+    mkdir -p "$HOME/Desktop/screencaps" 
+fi
+defaults write com.apple.screencapture location -string "$HOME/Desktop/screencaps"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
