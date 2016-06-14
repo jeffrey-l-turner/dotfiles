8738048e3a18c0d981d968e39bd700232aea8537
Jeff Turner <jturner@tillster.com>
2016-06-14 05:17:24 -0700

    including url comment

--- .git_template/config.sh
+++ .git_template/config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# see: http://tbaggery.com/2011/08/08/effortless-ctags-with-git.html
 # This is the configuration command for git and place in ~/.git_template/hooks
 git config --global init.templatedir '~/dotfiles/.git_template'
