b6c0fd067ad7a588fc99294dc3aa5175b96161d4
Jeff Turner <Jeffrey.L.Turner@me.com>
2017-01-09 21:06:03 -0800

    removing deprecated form

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
-{
+module.exports = {
+    "extends": "eslint:recommended",
+       "rules": {
+       // enable additional rules
+          "indent": ["error", 4],
+          "linebreak-style": ["error", "unix"],
+          "quotes": ["error", "double"],
+          "semi": ["error", "always"],
+
+          // override default options for rules from base configurations
+          "comma-dangle": ["error", "always"],
+          "no-cond-assign": ["error", "always"],
+
+          // disable rules from base configurations
+          "no-console": "off",
+      }
+
     "env": {
           "browser": true,
-          "node": true
+          "node": true,
+          "es6": true
     },
 
     "ecmaFeatures": {
