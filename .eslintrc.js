cda03b1a8034c7ddfe91fa8ed3b3ba6a94490dfd
jeffrey-l-turner <Jeffrey.L.Turner@me.com>
2018-04-13 00:15:56 -0700

    time to update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,104 +1,53 @@
 module.exports = {
-    'settings': {
-        'import/resolver': {
-        },
-    },
 
-    'extends': 'eslint:recommended',
+  'settings': {
+    'import/ignore': [
+      'node_modules',
+      '\\.json$',
+      '\\.(scss|less|css)$'
+    ],
+    'import/parser': 'babel-eslint',
+    'import/resolve': {
+      'extensions': [
+        '.js',
+        '.jsx',
+        '.json'
+      ]
+    }
+  },
 
-    'env': {
-        'browser': true,
-        'node': true,
-        'es6': true,
-    },
+  'plugins': [
+    'import',
+    'flowtype',
+    'filenames-suffix'
+  ],
 
-    'parserOptions': {
-        'ecmaVersion': 6,
-        'sourceType': 'module',
-    },
+  'extends': [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'eslint:recommended',
+   ],
 
-    'ecmaFeatures': {
-        'arrowFunctions': true,
-        'binaryLiterals': false,
-        'blockBindings': true,
-        'classes': true,
-        'defaultParams': true,
-        'destructuring': true,
-        'forOf': true,
-        'generators': true,
-        'modules': false,
-        'objectLiteralComputedProperties': true,
-        'objectLiteralDuplicateProperties': false,
-        'objectLiteralShorthandMethods': true,
-        'objectLiteralShorthandProperties': true,
-        'octalLiterals': false,
-        'regexUFlag': false,
-        'regexYFlag': false,
-        'restParams': true,
-        'spread': true,
-        'superInFunctions': true,
-        'templateStrings': true,
-        'unicodePointEscapes': true,
-        'globalReturn': false,
-        'jsx': true,
-    },
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true,
+    'jest': true,
+  },
 
-    'rules': {
-       // enable additional rules
-        'indent': ['error', 4,],
-        'linebreak-style': ['error', 'unix',],
-        'quotes': ['error', 'single',],
-        'semi': ['error', 'always',],
+  'globals': {
+    'document': true, 
+    '_': true,
+    '$': true,
+    'window': true, 
+  },
 
-          // override default options for rules from base configurations
-        'comma-dangle': ['error', 'always',],
-        'no-cond-assign': ['error', 'always',],
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+  },
 
-        // disable rules from base configurations
-        'no-console': 'off',
+}
 
-        // following Rules are divided into sections from http://eslint.org/docs/rules/
 
-        // Possible errors
-        'no-comma-dangle': 0,
 
-        // Best practices
-        'block-scoped-var': 2,
-        'complexity': [2, 10,],
-        'eqeqeq': [2, 'smart',],
-        'no-else-return': 2,
-        'no-floating-decimal': 2,
-        'wrap-iife': [2, 'any',],
-        'curly': 2,
-
-        // Variables
-        'no-use-before-define': 0,
-        'no-undef': 2,
-
-        // Node.js
-        'no-mixed-requires': 0,
-
-        // Stylistic issues
-        'brace-style': 2,
-        'camelcase': 0,
-        'new-cap': 0,
-        'no-lonely-if': 2,
-        'no-underscore-dangle': 0,
-        'keyword-spacing': 2,
-        'object-curly-spacing': [2, 'never',],
-        'space-infix-ops': 2,
-        'one-var': 0,
-        'no-irregular-whitespace': 2, 
-
-        // Legacy
-        'no-bitwise': 2,
-        'max-len': [2, 150, 4,],
-    },
-
-    'globals': {
-        'document': true, 
-        '_': true,
-        '$': true,
-        'window': true, 
-    },
-};
