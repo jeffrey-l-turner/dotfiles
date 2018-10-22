adcb1741e6c01559914606ed225c57921e176e64
Jeff Turner <jeffrey.l.turner@me.com>
2018-10-22 16:30:49 -0700

    initial, wip

---
+++ PowerLine.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# vim: ft=sh ts=2 sw=2 sts=2
+#
+# Inspired theme by:
+#  https://gist.github.com/3712874
+#  https://gist.github.com/kruton/8345450
+#
+DEBUG=0
+debug() {
+    if [[ ${DEBUG} -ne 0 ]]; then
+        >&2 echo -e "$@"
+    fi
+}
+# shellcheck disable=SC1091 disable=SC1090
+source ~/dotfiles/colordefs.sh
+
+CURRENT_BG='NONE'
+CURRENT_RBG='NONE'
+SEGMENT_SEPARATOR=''
+RIGHT_SEPARATOR=''
+LEFT_SUBSEG=''
+RIGHT_SUBSEG=''
+
+# Begin a segment
+# Takes two arguments, background and foreground. Both can be omitted,
+# rendering default background/foreground.
+_prompt_segment() {
+    local bg fg
+    declare -a codes
+
+    debug "Prompting $1 $2 $3"
+
+    # if commented out from kruton's original... I'm not clear
+    # if it did anything, but it messed up things like
+    # prompt_status - Erik 1/14/17
+
+    #    if [[ -z $1 || ( -z $2 && $2 != default ) ]]; then
+    codes=("${codes[@]}" $(text_effect reset))
+    #    fi
+    if [[ -n $1 ]]; then
+        bg=$(bg_color $1)
+        codes=("${codes[@]}" $bg)
+        debug "Added $bg as background to codes"
+    fi
+    if [[ -n $2 ]]; then
+        fg=$(fg_color $2)
+        codes=("${codes[@]}" $fg)
+        debug "Added $fg as foreground to codes"
+    fi
+
+    debug "Codes: "
+    # declare -p codes
+
+    if [[ $CURRENT_BG != NONE && $1 != $CURRENT_BG ]]; then
+        declare -a intermediate=($(fg_color $CURRENT_BG) $(bg_color $1))
+        debug "pre prompt " $(ansi intermediate[@])
+        PR="$PR $(ansi intermediate[@])$SEGMENT_SEPARATOR"
+        debug "post prompt " $(ansi codes[@])
+        PR="$PR$(ansi codes[@]) "
+    else
+        debug "no current BG, codes is $codes[@]"
+        PR="$PR$(ansi codes[@]) "
+    fi
+    CURRENT_BG=$1
+    [[ -n $3 ]] && PR="$PR$3"
+}
