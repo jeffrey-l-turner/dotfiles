c6a707bc5af882a90446051fffd954a58f9cf5ee
Jeff T <1573697+jeffrey-l-turner@users.noreply.github.com>
2021-02-14 15:49:50 -0800

    updating to 3/20 update: https://tbaggery.com/2011/08/08/effortless-ctags-with-git.html

--- .git_template/config.sh
+++ .git_template/config.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# see: http://tbaggery.com/2011/08/08/effortless-ctags-with-git.html
-# This is the configuration command for git and place in ~/.git_template/hooks
-git config --global init.templatedir '~/src/dotfiles/.git_template'
+#!/bin/sh
+.git/hooks/ctags >/dev/null 2>&1 &
