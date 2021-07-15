47be3e4c7d0ccaba29c0d5f40375b83f01e4bef1
Jeff T <1573697+jeffrey-l-turner@users.noreply.github.com>
2021-07-15 09:04:14 -0700

    updating to work w/curr rolling branch

--- lv-config.lua
+++ lv-config.lua
@@ -9,9 +9,6 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
--- on Mac: ` brew tap homebrew/cask-font`
---  `brew install font-jetbrains-mono-nerd-font font-jetbrains-mono font-fira-code-nerd-font font-blex-mono-nerd-font`
--- Remember to set non-ascii as well
 
 O.format_on_save = true
 O.completion.autocomplete = true
@@ -20,16 +17,15 @@ O.auto_close_tree = 0
 O.default_options.wrap = true
 O.default_options.timeoutlen = 100
 O.leader_key = " "
--- O.default_options.guifont = "JetBrainsMono Nerd Font:h15"
+O.default_options.guifont = "JetBrainsMono Nerd Font:h15"
 -- O.default_options.guifont = "BlexMono Nerd Font:h16"
-O.default_options.guifont = "Hack Nerd Font:h16"
+-- O.default_options.guifont = "Hack Nerd Font:h16"
 O.default_options.hlsearch = true
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 O.plugin.dashboard.active = true
-O.plugin.floatterm.active = true
-O.plugin.indent_line.active = true
+O.plugin.terminal.active = true
 O.plugin.zen.active = true
 O.plugin.zen.window.height = 0.75
 O.plugin.telescope.active = true
@@ -41,20 +37,45 @@ O.treesitter.ignore_install = { "haskell" }
 O.treesitter.highlight.enabled = true
 
 -- python
--- O.python.linter = 'flake8'
+-- O.lang.python.linter = 'flake8'
 O.lang.python.isort = true
 O.lang.python.diagnostics.virtual_text = true
 O.lang.python.analysis.use_library_code_types = true
+-- to change default formatter from yapf to black
+-- O.lang.python.formatter.exe = "black"
+-- O.lang.python.formatter.args = {"-"}
+
+-- go
+-- to change default formatter from gofmt to goimports
+-- O.lang.formatter.go.exe = "goimports"
 
 -- javascript
 O.lang.tsserver.linter = nil
--- O.lang.tsserver.formatter = "eslint" -- ?
 
 -- rust
-O.lang.rust.formatter = {
-  exe = "rustfmt",
-  args = {"--emit=stdout"},
-}
+-- O.lang.rust.formatter = {
+--   exe = "rustfmt",
+--   args = {"--emit=stdout", "--edition=2018"},
+-- }
+
+--LaTeX
+-- Options: https://github.com/latex-lsp/texlab/blob/master/docs/options.md
+O.lang.latex.active = true
+O.lang.latex.aux_directory = "."
+O.lang.latex.bibtex_formatter = "texlab"
+O.lang.latex.build.args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" }
+O.lang.latex.build.executable = "latexmk"
+O.lang.latex.build.forward_search_after = false
+O.lang.latex.build.on_save = false
+O.lang.latex.chktex.on_edit = false
+O.lang.latex.chktex.on_open_and_save = false
+O.lang.latex.diagnostics_delay = 300
+O.lang.latex.formatter_line_length = 80
+O.lang.latex.forward_search.executable = "zathura"
+O.lang.latex.latex_formatter = "latexindent"
+O.lang.latex.latexindent.modify_line_breaks = false
+-- O.lang.latex.auto_save = false
+-- O.lang.latex.ignore_errors = { }
 
 -- Additional Plugins
 -- O.user_plugins = {
