43083f4318645774c61c01e6561f2c0cfe2788d1
Jeff Turner <jlt@crypteriat.org>
2021-11-18 12:55:40 -0800

    updating to be compat w/latest rolling

--- config.lua
+++ config.lua
@@ -17,50 +17,50 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Gui Font for Lunar Vide setup:
 vim.opt.guifont = "JetBrainsMono Nerd Font:h15"
 -- see: https://github.com/LunarVim/LunarVim/issues/1607
-lvim.lang.typescript.formatters = { { exe = "prettier"  } }
-lvim.lang.typescriptreact.formatters = { { exe = "prettier"  } }
+-- lvim.lang.typescript.formatters = { { exe = "prettier"  } }
+-- lvim.lang.typescriptreact.formatters = { { exe = "prettier"  } }
 
--- Must run :set manually when in gui; command below originally worked -- cannot now find default font option in config
--- Can change ~/.local/share/lunarvim/lvim/lua/settings.lua to proper font
--- O.default_options.guifont = "JetBrainsMono Nerd Font:h14"
--- :set guifont=JetBrainsMono\ Nerd\ Font:h14
+-- must run :set manually when in gui; command below originally worked -- cannot now find default font option in config
+-- can change ~/.local/share/lunarvim/lvim/lua/settings.lua to proper font
+-- o.default_options.guifont = "jetbrainsmono nerd font:h14"
+-- :set guifont=jetbrainsmono\ nerd\ font:h14
 
--- keymappings [view all the defaults by pressing <leader>Lk]
+-- keymappings [view all the defaults by pressing <leader>lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":wa<cr>"
+lvim.keys.normal_mode["<c-s>"] = ":wa<cr>"
 -- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
+-- lvim.keys.normal_mode["<c-up>"] = ""
 -- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+-- lvim.keys.normal_mode["<c-q>"] = ":q<cr>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- change telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- lvim.builtin.telescope.on_config_done = function()
 --   local actions = require "telescope.actions"
 --   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   lvim.builtin.telescope.defaults.mappings.i["<c-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<c-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<c-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<c-p>"] = actions.cycle_history_prev
 --   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.n["<c-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<c-k>"] = actions.move_selection_previous
 -- end
 
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["p"] = { "<cmd>telescope projects<cr>", "projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+--   name = "+trouble",
+--   r = { "<cmd>trouble lsp_references<cr>", "references" },
+--   f = { "<cmd>trouble lsp_definitions<cr>", "definitions" },
+--   d = { "<cmd>trouble lsp_document_diagnostics<cr>", "diagnosticss" },
+--   q = { "<cmd>trouble quickfix<cr>", "quickfix" },
+--   l = { "<cmd>trouble loclist<cr>", "locationlist" },
+--   w = { "<cmd>trouble lsp_workspace_diagnostics<cr>", "diagnosticss" },
 -- }
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+-- todo: user config for predefined plugins
+-- after changing plugin config exit and reopen lunarvim, run :packerinstall :packercompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 
@@ -69,65 +69,65 @@ lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
+-- generic lsp settings
 -- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- see <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 --   local function buf_set_option(...)
 --     vim.api.nvim_buf_set_option(bufnr, ...)
 --   end
---   --Enable completion triggered by <c-x><c-o>
+--   --enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 -- you can overwrite the null_ls setup table (useful for setting the root_dir function)
 -- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+--   root_dir = require("lspconfig").util.root_pattern("makefile", ".git", "node_modules"),
 -- }
 -- or if you need something more advanced
 -- lvim.lsp.null_ls.setup.root_dir = function(fname)
 --   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--     return require("lspconfig/util").root_pattern("makefile", ".git", "node_modules")(fname)
 --       or require("lspconfig/util").path.dirname(fname)
 --   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--     return require("lspconfig/util").root_pattern("makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
 --   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--     return require("lspconfig/util").root_pattern("makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
 --   end
 -- end
 
-lvim.lang.javascript.formatters = {
-  {
-    exe = "prettier",
-    args = {}
-  }
-}
+-- lvim.lang.javascript.formatters = {
+--   {
+--     exe = "prettier",
+--     args = {}
+--   }
+-- }
 
-lvim.lang.javascript.linters = {
-  {
-    exe = "eslint_d",
-    args = {}
-  }
-}
-lvim.lang.sh.formatters = {
-  -- on Mac brew install shellharden
-  {
-    exe = "shellharden",
-    args = {}
-  }
-}
-lvim.lang.python.formatters = {
-  {
-    exe = "black",
-    args = {}
-  }
-}
--- set an additional linter
-lvim.lang.python.linters = {
-  {
-    exe = "flake8",
-    args = {}
-  }
-}
+-- lvim.lang.javascript.linters = {
+--   {
+--     exe = "eslint_d",
+--     args = {}
+--   }
+-- }
+-- lvim.lang.sh.formatters = {
+--   -- on mac brew install shellharden
+--   {
+--     exe = "shellharden",
+--     args = {}
+--   }
+-- }
+-- lvim.lang.python.formatters = {
+--   {
+--     exe = "black",
+--     args = {}
+--   }
+-- }
+-- -- set an additional linter
+-- lvim.lang.python.linters = {
+--   {
+--     exe = "flake8",
+--     args = {}
+--   }
+-- }
 
 -- Additional Plugins
 lvim.plugins = {
