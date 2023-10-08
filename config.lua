dce894eb7fe21a477b2a3c76e9c54fa0fff7d510
jlt <noreply>
2023-10-08 10:36:00 -0700

    updating from fresh install with links

--- config.lua
+++ config.lua
@@ -13,7 +13,11 @@ filled in as strings with either
 a global executable or a path to
 an executable
 ]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 
 -- general
 lvim.log.level = "warn"
