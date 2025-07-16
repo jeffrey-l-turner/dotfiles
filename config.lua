db66b7b401a38ae7ff84a3d3e14f7d3f305db49e
Jeff Turner <1573697+jeffrey-l-turner@users.noreply.github.com>
2025-07-16 16:59:32 -0700

    adding rho setup

--- config.lua
+++ config.lua
@@ -231,6 +231,19 @@ lvim.plugins = {
     config = true,
     cmd = "Glow"
   },
+  { 'F1R3FLY-io/rholang-nvim',
+  config = function()
+    require('rholang').setup()
+  end,
+  build = function()
+    local cmd = 'cd ' .. vim.fn.expand('<sfile>:p:h') .. ' && make && make install'
+    local result = vim.fn.system(cmd)
+    if vim.v.shell_error ~= 0 then
+      vim.notify('rholang-nvim build failed: ' .. result, vim.log.levels.ERROR)
+    else
+      vim.notify('rholang-nvim build successful', vim.log.levels.INFO)
+    end
+  end,
   {
       "tzachar/cmp-tabnine",
       build = "./install.sh",
@@ -246,6 +259,7 @@ lvim.plugins = {
       lazy = true,
       event = "InsertEnter",
     }
+}
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
